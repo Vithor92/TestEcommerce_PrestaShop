5484c00c84643974c9674693520465938c412f23
Vithor92 <vithorjp92@hotmail.com>
2021-10-19 13:11:58 -0300

    Primeiro commit de script de teste do prestashop

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,36 @@
+package base;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTests {
+
+    private static WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeAll
+    public static void inicializar(){
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver\\version-94\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @BeforeEach
+    public void carregarPaginaInicial(){
+        driver.get("https://marcelodebittencourt.com/demoprestashop/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        homePage = new HomePage(driver);
+    }
+
+    @AfterAll
+    public static void finalizarNavegador(){
+        driver.quit();
+    }
+
+}
